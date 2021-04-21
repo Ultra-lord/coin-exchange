a7d46d9a0453fc8d09f5e6f99b08a9e084ee03c7
Jonathan Ballinger <jonathanballinger67@gmail.com>
2021-04-20 21:06:08 -0700

    updated tbody with map/ updated state

--- src/App.js
+++ src/App.js
@@ -5,33 +5,71 @@ import Coin from './components/Coin/Coin';
 import AccountBalance from './components/AccountBalance/AccountBalance'
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="React logo" className="App-logo" />
-        <h1 className="App-title">
-          Coin Exchange 
-        </h1>
-      </header>
-      <AccountBalance amount = {10000} />
-      <table className="coin-table">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Ticker</th>
-          <th>Price</th>
-        </tr>
-      </thead>
-      <tbody>
-        <Coin name="Bitcoin" ticker="BTC" price={9999.99} />
-        <Coin name="Ethereum" ticker="ETH" price={2999.99} />
-        <Coin name="Tether" ticker="USDT" price={1.0} />
-        <Coin name="Ripple" ticker="XRP" price={1.3} />
-      </tbody>
-    </table>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      balance: 10000,
+      coinData: [
+
+        {
+          name: 'Bitcoin',
+          ticker: 'Btc',
+          price: 9999.99
+        },
+        {
+          name: 'Ethereum',
+          ticker: 'Eth',
+          price: 2999.99
+        },
+        {
+          name: 'Tether',
+          ticker: 'USDT',
+          price: 1.0
+        },
+        {
+          name: 'Ripple',
+          ticker: 'XRP',
+          price: 1.3
+        },
+        {
+          name: 'Bitcoin Cash',
+          ticker: 'BCH',
+          price: 298.99
+        }
+      ]
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} alt="React logo" className="App-logo" />
+          <h1 className="App-title">
+            Coin Exchange 
+          </h1>
+        </header>
+        <AccountBalance amount = {this.state.balance} />
+        <table className="coin-table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Ticker</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            this.state.coinData.map( ({name, ticker, price}) => 
+            <Coin key={ticker} name={name} ticker={ticker} price={price} />,
+            )
+          }
+        </tbody>
+      </table>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
