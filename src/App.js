02ba04e16f1e2a12a3eb8febbd228346ead9a526
Jonathan Ballinger <jonathanballinger67@gmail.com>
2021-04-21 12:43:19 -0700

    lifting state up

--- src/App.js
+++ src/App.js
@@ -41,16 +41,35 @@ class App extends React.Component {
           name: 'Bitcoin Cash',
           ticker: 'BCH',
           price: 298.99
-        }
+        },
       ]
     }
+    this.handleRefresh = this.handleRefresh.bind(this);
   }
+  handleRefresh(valueChangeTicker) {
+   const newCoinData = this.state.coinData.map( function( {ticker, name, price} ) {
+     let newPrice = price;
+    if (valueChangeTicker === ticker) {
+      const randomPercentage = 0.995 + Math.random() * 0.01;
+      newPrice = newPrice * randomPercentage;
+        }
+        return {
+          ticker,
+          name,
+          price: newPrice
+        }
+
+      });
+      
+      this.setState({ coinData: newCoinData });
+     }
+
   render() {
     return (
       <Content>
         <Header />
         <AccountBalance amount = {this.state.balance} />
-       <CoinList coinData={this.state.coinData} />
+       <CoinList coinData={this.state.coinData} handleRefresh={this.handleRefresh} />
       </Content>
     );
   }
