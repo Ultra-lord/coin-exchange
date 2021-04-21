6a326ed14820d7b8dae188527a2445e360f07a19
Jonathan Ballinger <jonathanballinger67@gmail.com>
2021-04-20 20:27:18 -0700

    styled components update

--- src/components/AccountBalance/AccountBalance.jsx
+++ src/components/AccountBalance/AccountBalance.jsx
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import './AccountBalance.css' 
+import PropTypes from 'prop-types'; 
+import styled from 'styled-components'
 
+const Section = styled.section`
+    font-size: 2rem;
+    text-align: left;
+    padding: 1.5rem  0 1.5rem 5rem;
+
+`;
 export default class AccountBalance extends Component {
     render() {
         return (
-            <section className="balance">
-              ${this.props.amount}  
-            </section>
+            <Section>
+             Balance: ${this.props.amount}  
+            </Section>
         );
     }
 }
